7313fe8ba9e4642160d3297bd70187a93dd64c5e
kapoorlab <randomaccessiblekapoor@gmail.com>
2022-10-28 16:52:39 +0200

    initial commit

---
+++ src/tracking_challenge_datareader/_tests/test_sample_data.py
@@ -0,0 +1,7 @@
+# from tracking_challenge_datareader import make_sample_data
+
+# add your tests here...
+
+
+def test_something():
+    pass
---
+++ src/tracking_challenge_datareader/_tests/test_writer.py
@@ -0,0 +1,7 @@
+# from tracking_challenge_datareader import write_single_image, write_multiple
+
+# add your tests here...
+
+
+def test_something():
+    pass
